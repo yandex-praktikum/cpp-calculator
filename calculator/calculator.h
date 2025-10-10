85e57836641953a8daa75726bd8870e61aba1baf
ash108drush <47239895+ash108drush@users.noreply.github.com>
2025-10-10 16:15:54 +0300

    Update calculator.h

--- calculator/calculator.h
+++ calculator/calculator.h
@@ -1,1 +1,23 @@
 // В этом файле объявления функций.
+#pragma once
+
+using Number = double;
+
+enum class Operations{
+    SUM,
+    DIFF,
+    MULT,
+    DIV,
+    POW,
+    MEM,
+    LOAD,
+    CALC,
+    QUIT,
+    SET,
+    CLEAR,
+
+};
+
+bool ReadNumber(Number &result);
+bool ReadOperation(int &oper);
+bool RunCalculatorCycle();
